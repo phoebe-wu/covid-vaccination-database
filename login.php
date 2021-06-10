16c3c16443dd62433be659b86cd58dad308e05a8
Annie Wang <zwang103@students.cs.ubc.ca>
2021-06-09 17:56:11 -0700

    Linked register and login pages with MySQL DB, Added session cookie on login page.

---
+++ login.php
@@ -0,0 +1,45 @@
+<?php
+session_start();
+require 'connect.php';
+
+
+
+function handleLoginRequest($conn) {
+
+    $userid = $_POST['userid'];
+    $password = $_POST['password'];
+    $_SESSION['userid'] = $userid;
+    if (($userid == '') || ($password == '')) {
+        header("refresh:2; url='login.html'");
+        echo "<br>Email or password cannot be empty. Auto-refresh in 1 second.<br>";
+        exit;
+    }
+    $sql = "SELECT Count(*) FROM Patient WHERE (Patient.user_ID='$userid' and Patient.PASSWORD='$password')";
+    $result = mysqli_query($conn, $sql);
+    $num = ($result->fetch_array())[0];
+
+    if ($num == 1) {
+        echo "<br>Logged In Successfully!<br>";
+        header('refresh:0.5; url=patient/index.html');
+    } else if ($num == 0) {
+        header('refresh:2; url=login.html');
+        echo "<br>Email or password wrong. Auto-refresh in 1 seconds.<br>";
+    }
+    # CloseCon($conn);
+}
+
+
+function handlePOSTRequest() {
+    $conn = OpenCon();
+    if (array_key_exists('login', $_POST)) {
+
+        handleLoginRequest($conn);
+    }
+    CloseCon($conn);
+}
+
+if (isset($_POST['login'])) {
+
+    handlePOSTRequest();
+}
+
---
+++ reg.php
@@ -0,0 +1,55 @@
+<?php
+require 'connect.php';
+
+
+
+function handleRegisterRequest($conn) {
+
+    $name = $_POST["name"];
+    $userid = $_POST["userid"];
+    $phn = $_POST["phn"];
+    $address = $_POST["address"];
+    $pwd = $_POST["password"];
+    $cpwd = $_POST["cpassword"];
+    $age = $_POST["age"];
+
+
+    if( ($name == '') || ($userid == '') || ($phn == '') ||($address == '') || ($pwd == '') ||($cpwd == '') || ($age == '')) {
+        header("refresh:2; url='register.html'");
+        echo "<br>All fields are required. Auto-refresh in 1 second.<br>";
+        exit;
+    }
+
+    if (strcmp($pwd, $cpwd) !== 0) {
+        header("refresh:2; url='register.html'");
+        echo '<br>two passwords do not match. Auto-refresh in 1 second.<br>" ';
+        exit;
+    }
+    $sql = "INSERT INTO Patient VALUES('$userid', '$pwd', '$name', '$age', '$phn', '$address')";
+    $result = mysqli_query($conn, $sql);
+    if ($result == false) {
+        header("refresh:2; url='register.html'");
+        echo "<br>Register Unsuccessfully, try again. Auto-refresh in 1 second.<br>";
+        exit;
+    } else {
+        header('refresh:2; url=login.html');
+        echo "<br>Registered Successfully! Please Log in<br>";
+
+    }
+}
+
+
+function handleRegPOSTRequest() {
+    $conn = OpenCon();
+    if (array_key_exists('register', $_POST)) {
+
+        handleRegisterRequest($conn);
+    }
+    CloseCon($conn);
+}
+
+
+if (isset($_POST['register'])) {
+    handleRegPOSTRequest();
+}
+

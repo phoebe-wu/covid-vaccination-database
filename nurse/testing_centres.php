d09409011caecf576e5cc28ec16e39072e5e738a
Annie Wang <zwang103@students.cs.ubc.ca>
2021-06-16 20:56:01 -0700

    Update testing_centres.php

--- nurse/testing_centres.php
+++ nurse/testing_centres.php
@@ -282,7 +282,7 @@ $conn = OpenCon();
                                             }
                                         }
                                     }
-								   $result = $conn->query($sql);
+//								   $result = $conn->query($sql);
 								   if ($result->num_rows > 0) {
 								   // output data of each row
 								   while($row = $result->fetch_assoc()) {

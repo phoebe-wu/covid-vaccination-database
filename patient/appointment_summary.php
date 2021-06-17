aa860039478a9deec3b14eb96c55233ae9f7f9a8
em357 <em357@students.cs.ubc.ca>
2021-06-16 21:53:46 -0700

    fix link in summary page

--- patient/appointment_summary.php
+++ patient/appointment_summary.php
@@ -50,7 +50,7 @@
                         <li class="sidebar-title">Menu</li>
 
                         <li class="sidebar-item ">
-                            <a href="../index.php" class='sidebar-link'>
+                            <a href="index.php" class='sidebar-link'>
                                 <i class="bi bi-grid-fill"></i>
                                 <span>Dashboard</span>
                             </a>
